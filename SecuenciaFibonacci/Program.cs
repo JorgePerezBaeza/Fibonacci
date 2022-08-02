9974cbce97a60d6253b827cb0c1f06c10014f881
Jorge Pérez Baeza <92327870+JorgePerezBaeza@users.noreply.github.com>
2022-08-02 00:02:50 -0600

    ArrayDinamico

--- SecuenciaFibonacci/Program.cs
+++ SecuenciaFibonacci/Program.cs
@@ -7,21 +7,24 @@ namespace SecuenciaFibonacci
         static void Main(string[] args)
         {
             //Mostrar la serie de Fibonacci
-            Console.WriteLine("Primeros 15 numeros de la Serie de Fibonacci");
+            int NumF;
+            Console.WriteLine("¿Cuantos numeros de la Serie de Fibonacci quieres mostrar?");
+            NumF = int.Parse(Console.ReadLine());
+            Console.WriteLine("Mostrando " + NumF + " numeros de la Serie de Fibonacci");
             int x = 1;
             int y = 0;
             int contador2 = 1;
-            int[] array = new int[14];
+            int[] array = new int[NumF];
             Console.Write(1);
             Console.Write(",");
-            for (int contador = 0; contador < array.Length; contador++)
+            for (int contador = 0; contador < (array.Length - 1); contador++)
             {
                 array[contador] = x + y;
                 y = x;
                 x = array[contador];
                 Console.Write(array[contador]);
                 contador2 += 1;
-                if (contador2 <= array.Length)
+                if (contador2 < array.Length)
                 {
                     Console.Write(",");
                 }
@@ -34,7 +37,7 @@ namespace SecuenciaFibonacci
             //Buscar un numero de la serie de Fibonacci
             Console.WriteLine("");
             int a, b, limite, i, auxiliar;
-            Console.WriteLine("Ingrese el número de la serie de Fibonacci que desea mostrar");
+            Console.WriteLine("Ingrese el número de la serie de Fibonacci que desea buscar");
             limite = int.Parse(Console.ReadLine());
             a = 0;
             b = 1;
